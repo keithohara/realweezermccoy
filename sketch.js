52b7d96a3d42d6677af776ae914ab26a236be719
Keith O'Hara <kohara@bard.edu>
2021-10-23 14:50:40 -0400

    directions

--- sketch.js
+++ sketch.js
@@ -10,11 +10,10 @@ function setup() {
     for (let s of music){
         s.playMode('restart');
     }
-
 }
 
 function mousePressed() {
-  // playing a sound file on a user gesture
+    // playing a sound file on a user gesture
     // is equivalent to `userStartAudio()`
     for (let s of music){
 	s.loop(.5, 1, 1);
@@ -23,15 +22,14 @@ function mousePressed() {
 
 
 function draw() {
-  background(0);
-  let amplitude = map(mouseX, 0, width, 0, 1.0);
-  music[0].setVolume(amplitude);
-  music[1].setVolume(1.0 - amplitude);
-
-  tint(255, amplitude * 255);
-  image(images[0], 0, 0, width, height);
-  tint(255, (1-amplitude) * 255);
-  image(images[1], 0, 0, width, height);
-
+    background(0);
+    let amplitude = map(mouseX, 0, width, 0, 1.0);
+    music[0].setVolume(amplitude);
+    music[1].setVolume(1.0 - amplitude);
+    
+    tint(255, amplitude * 255);
+    image(images[0], 0, 0, width, height);
+    tint(255, (1-amplitude) * 255);
+    image(images[1], 0, 0, width, height); 
 }
 
